c6ec209a634e3f6675627a6e335f7a42d8fad46c
1435775329 <43976856+1435775329@users.noreply.github.com>
2022-04-30 05:24:12 -0700

    First commit

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const SAT_API_KEY = "ELG7FA-NL3U4H-FAW6ZB-4URS";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
